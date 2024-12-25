31c120a17094d189d64511fc62345046ce879647
Darkosylver <darkosylver27@gmail.com>
2024-12-25 11:31:14 +0200

    Fixed the input

--- eigen.py
+++ eigen.py
@@ -2,14 +2,27 @@ import sympy as sym
 import matplotlib as plot
 import numpy as num
 
-matrixSize = int(input("Enter the matrix size"))
-matrix = []
+matrixSize = int(input("Enter the matrix size: "))
+matrix = num.empty((0,matrixSize), int)
 
-print("Enter each row in the matrix")
+print("Enter each entry in a single line, separate numbers by space: ")
 
-for Row in range(matrixSize):
-    row = []
-    for Column in range(matrixSize):
-        row.append(int(input()))
-    matrix.append(row)
+entries = list(map(int, input().split()))
+
+matrix = num.array(entries).reshape(matrixSize, matrixSize)
+
+print(matrix)
+
+initialValue = num.empty((0), float)
+
+print("Enter Initial Value, same as the matrix: ")
+
+entries = list(map(float, input().split()))
+
+initialValue = num.array(entries).reshape(matrixSize,1)
+
+print(initialValue)
+
+
+
 
