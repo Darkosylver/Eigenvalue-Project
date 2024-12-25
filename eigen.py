41c5007ed6aba8f8620f2ac71bd9e9274cf11597
Darkosylver <darkosylver27@gmail.com>
2024-12-25 12:00:58 +0200

    Power Method

--- eigen.py
+++ eigen.py
@@ -2,8 +2,20 @@ import sympy as sym
 import matplotlib as plot
 import numpy as num
 
+def powerMethod(matrix, matrixSize, initialValue):
+    eigenVector = num.empty((1,matrixSize), float)
+    eigenValue = 0
+    while not num.array_equal(eigenVector,initialValue):
+        eigenVector = num.dot(matrix,initialValue)
+        eigenValue = num.max(eigenVector)
+        eigenVector = num.divide(eigenVector, eigenValue)
+        initialValue = eigenVector
+    print("Eigen Value is", eigenValue)
+    print("Eigen Vector is")
+    print(eigenVector)
+
 matrixSize = int(input("Enter the matrix size: "))
-matrix = num.empty((0,matrixSize), int)
+matrix = num.empty((matrixSize,matrixSize), int)
 
 print("Enter each entry in a single line, separate numbers by space: ")
 
@@ -13,7 +25,7 @@ matrix = num.array(entries).reshape(matrixSize, matrixSize)
 
 print(matrix)
 
-initialValue = num.empty((0), float)
+initialValue = num.empty((1,matrixSize), float)
 
 print("Enter Initial Value, same as the matrix: ")
 
@@ -22,6 +34,12 @@ entries = list(map(float, input().split()))
 initialValue = num.array(entries).reshape(matrixSize,1)
 
 print(initialValue)
+
+powerMethod(matrix,matrixSize,initialValue)
+
+
+
+
 
 
 
